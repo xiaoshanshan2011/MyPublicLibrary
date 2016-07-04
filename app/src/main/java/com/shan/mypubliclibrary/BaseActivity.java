d0c08c15b7f8a5239e8c0fcf0a6b1410e9560358
xiaochen2016 <13422058465@163.com>
2016-07-05 00:07:42 +0800

    增加图片工具类

---
+++ app/src/main/java/com/shan/mypubliclibrary/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.shan.mypubliclibrary;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by 陈俊山 on 4/7/2559.
+ */
+public class BaseActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
