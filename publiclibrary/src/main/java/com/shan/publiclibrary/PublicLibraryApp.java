01f609ab4b659f9bed1896167e571e5410cae39e
xiaochen2016 <13422058465@163.com>
2016-07-04 13:21:09 +0800

    增加PublicLibrary.class

---
+++ publiclibrary/src/main/java/com/shan/publiclibrary/PublicLibraryApp.java
@@ -0,0 +1,13 @@
+package com.shan.publiclibrary;
+
+import android.app.Application;
+
+/**
+ * Created by 陈俊山 on 2016/7/4.
+ */
+public class PublicLibraryApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
