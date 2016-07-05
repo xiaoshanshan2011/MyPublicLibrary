7964fb156ee9dc40bbf0e61025200834ef56f05a
xiaochen2016 <13422058465@163.com>
2016-07-06 00:07:57 +0800

    1111111

--- publiclibrary/src/main/java/com/shan/publiclibrary/config/Constants.java
+++ publiclibrary/src/main/java/com/shan/publiclibrary/config/Constants.java
@@ -4,5 +4,7 @@ package com.shan.publiclibrary.config;
  * Created by 陈俊山 on 2016/7/5.
  */
 public class Constants {
+    public static final int WHAT_SUCCESS = 1;
+    public static final int WHAT_FAILURE = -1;
 
 }
